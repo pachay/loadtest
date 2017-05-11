cb6bc8da8e5a94be3112b115921825fe4535ce92
pachay <apachay@octoberry.ru>
2017-05-11 18:31:59 +0300

    init

---
+++ client.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+import websocket
+import json
+import time
+import os
+
+
+# ws_url = "ws://chatdemo:8888/chatsocket"
+ws_url = os.getenv('WS_URL')
+n_clients = int(os.getenv('N_CLIENTS'))
+
+# ws.send(json.dumps({'body': '123456722222'}))
+
+class Client(object):
+    def __init__(self, ws_url):
+        self.ws = websocket.WebSocket()
+        self.ws.connect(ws_url)
+
+    def send(self):
+        self.ws.send(json.dumps({'body': u'Привет, это чат!'}))
+
+clients = []
+for n in range(0, n_clients):
+    clients.append(Client(ws_url))
+
+while True:
+    for client in clients:
+        client.send()
+        print 'Message sent'
+        time.sleep(0.1)
+
